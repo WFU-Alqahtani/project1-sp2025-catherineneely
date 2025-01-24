f13c974a76aa07464fa3cafeefbd9dde6f61c9cf
catherineneely <neelc24@wfu.edu>
2025-01-24 16:03:06 -0500

    created createCart method to create the user's cart based on their inputs

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Item[] store = setUpStore();
+        ArrayList<Item> cart = createCart(store, args);
 
     }
+
     public static Item[] setUpStore() {
         Item[] store = new Item[5];
         store[0] = new Item("Vanilla Ice Cream", 6.0);
@@ -17,4 +20,19 @@ public class Main {
         store[4] = new Item("Strawberry Ice Cream", 2.5);
         return store;
     }
+    public static ArrayList<Item> createCart(Item[] store, String[] args) {
+        int numberOfItems = Integer.parseInt(args[0]);
+        int[] items = new int[numberOfItems];
+        for (int i = 0; i < numberOfItems; i++) {
+            items[i] = Integer.parseInt(args[i + 1]);
+        }
+        ArrayList<Item> cart = new ArrayList<Item>();
+        for (int i = 0; i < items.length; i++) {
+            cart.add(store[items[i]]);
+        }
+        return cart;
+    }
+
 }
+
+//             System.out.println(cart.get(i).getItemName());
