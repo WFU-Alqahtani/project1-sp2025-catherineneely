67e5e8aafef5d7d23291332a12c65ba069046944
catherineneely <neelc24@wfu.edu>
2025-01-27 21:07:36 -0500

    added comments explaining the purpose of each method, as well as some of the for loops

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         printReceiptInOrder(cart);
         emptyCartReverseOrder(cart);
     }
-
+    // The setUpStore method sets up the ice cream flavors in the store using the Item object.
     public static Item[] setUpStore() {
         Item[] store = new Item[5];
         store[0] = new Item("Vanilla Ice Cream       ", 2.5);
@@ -21,18 +21,25 @@ public class Main {
         store[4] = new Item("Strawberry Ice Cream    ", 3.0);
         return store;
     }
+    // The creatCart method creates the user's cart from the arguments input in the terminal, with
+    // the first number being the total number of items and the following numbers corresponding
+    // to the flavors.
     public static ArrayList<Item> createCart(Item[] store, String[] args) {
         int numberOfItems = Integer.parseInt(args[0]);
         int[] items = new int[numberOfItems];
+        // This loop fills the "items" array with the arguments from terminal.
         for (int i = 0; i < numberOfItems; i++) {
-            items[i] = Integer.parseInt(args[i + 1]);
+                items[i] = Integer.parseInt(args[i + 1]);
         }
         ArrayList<Item> cart = new ArrayList<>();
+        // This loop adds the items to the "cart" ArrayList.
         for (int i = 0; i < items.length; i++) {
             cart.add(store[items[i]]);
         }
         return cart;
     }
+    // The printReceiptInOrder method prints the items in the user's cart and their price.
+    // It then calculates and prints the subtotal, tax, and total cost.
     public static void printReceiptInOrder(ArrayList<Item> cart) {
         System.out.println("""
                 Receipt
@@ -54,6 +61,8 @@ public class Main {
                 (c) Total: %.2f
                 """, subtotal, total);
     }
+    // The emptyCartReverseOrder method empties the user's cart by removing the items in the
+    // backwards order they were put into the cart.
     public static void emptyCartReverseOrder(ArrayList<Item> cart) {
         System.out.println("\nRemoving all items from the cart in backwards order...");
         for(int i = cart.size() - 1; i >= 0; i--) {
